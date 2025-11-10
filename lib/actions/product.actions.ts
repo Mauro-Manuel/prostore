b98702070a4183b5589654773f9aed3e26f7a7cb
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-11-10 16:07:33 +0100

    first commit

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,33 @@
+'use server'
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { PrismaClient } from '@prisma/client';
+import { convertToPlainObject } from "../utils"
+//import { prisma } from "@/db/prisma";
+
+// Get latest products
+export async function getLatestProducts() {
+
+ const prisma = new PrismaClient();
+
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: 'desc' },
+  });
+
+  const normalized = data.map((p) => ({
+    ...p,
+    price: p.price.toString(),
+    rating: p.rating?.toString() ?? "0",
+  }));
+
+  return convertToPlainObject(normalized);
+}
+
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+   const prisma = new PrismaClient();
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
