f44e18f130ed22cfb785fb6719dc93b38bc63732
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-11-15 23:20:29 +0100

    feat: smooth ui with useTransation hook

--- components/product/add-to-cart.tsx
+++ components/product/add-to-cart.tsx
@@ -3,24 +3,24 @@
 import { Cart, CartItem } from "@/types/index";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
-import { Plus, Minus, PlusCircle } from "lucide-react";
+import { Plus, Minus, PlusCircle, Loader } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "../ui/toast";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
-import { startTransition } from "react";
+import { useTransition } from "react";
 
-
-const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
   const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
 
- const handleAddToCart = async () => {
+  const handleAddToCart = async () => {
     startTransition(async () => {
       const res = await addItemToCart(item);
 
       if (!res.success) {
         toast({
-          variant: 'destructive',
+          variant: "destructive",
           description: res.message,
         });
         return;
@@ -31,9 +31,9 @@ const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
         description: res.message,
         action: (
           <ToastAction
-            className='bg-primary text-white hover:bg-gray-800'
-            altText='Go To Cart'
-            onClick={() => router.push('/cart')}
+            className="bg-primary text-white hover:bg-gray-800"
+            altText="Go To Cart"
+            onClick={() => router.push("/cart")}
           >
             Go To Cart
           </ToastAction>
@@ -41,14 +41,14 @@ const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
       });
     });
   };
-  
+
   // Handle remove from cart
   const handleRemoveFromCart = async () => {
     startTransition(async () => {
       const res = await removeItemFromCart(item.productId);
 
       toast({
-        variant: res.success ? 'default' : 'destructive',
+        variant: res.success ? "default" : "destructive",
         description: res.message,
       });
 
@@ -57,26 +57,41 @@ const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
   };
 
   // check if items is in cart
-  const  existItem = cart && cart.items.find((x) => x.productId === item.productId);
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
   /*return (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
        <PlusCircle />  Add to Cart
     </Button>
   );*/
-     return existItem ? ( <div>
-      <Button type="button" variant='outline' onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
-      <Button type="button" variant='outline' onClick={handleAddToCart}>
-        <Plus className="h-4 w-4" />
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
       </Button>
-     </div>) : ( 
-      <Button className="w-full" type="button" onClick={handleAddToCart}>
-        <PlusCircle /> Add to Cart
-      </Button>
-    )
- 
+    </div>
+  ) : (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <PlusCircle className="w-4 h-4" />
+      )}{" "}
+      Add To Cart
+    </Button>
+  );
 };
 
 export default AddToCart;
