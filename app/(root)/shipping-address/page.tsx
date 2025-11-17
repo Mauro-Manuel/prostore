db93cebfc42ad0d3ef26d221a84e2b68d84c0bc0
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-11-17 21:39:35 +0100

    feat: shipping address & form and update user Address

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -4,25 +4,30 @@ import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
 import { getUserById } from "@/lib/actions/users.actions";
+import ShippingAddressForm from "./shipping-address-form";
 
 export const metadata: Metadata = {
   title: "Shipping Address",
-}
+};
 
 const ShippingAddressPage = async () => {
   const cart = await getMyCart();
 
-   if (!cart || cart.items.length === 0) redirect('/cart');
+  if (!cart || cart.items.length === 0) redirect("/cart");
 
-   const session = await auth();
+  const session = await auth();
 
-   const userId = session?.user?.id;
+  const userId = session?.user?.id;
 
-   if(!userId) throw new Error("No user ID");
+  if (!userId) throw new Error("No user ID");
 
-   const user = await getUserById(userId)
+  const user = await getUserById(userId);
 
-  return <>Address</>;
-}
- 
+  return (
+    <>
+      <ShippingAddressForm address={user.address as ShippingAddress} />
+    </>
+  );
+};
+
 export default ShippingAddressPage;
