7a16214101d1e38473d5ca6d591e51ca72b1a593
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-11-24 20:21:49 +0100

    feat: order datails table

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,11 +1,13 @@
-import { Metadata } from 'next';
-import { notFound} from 'next/navigation';
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 //import { ShippingAddress } from '@/types';
-import { getOrderById } from '@/lib/actions/order.actions';
-import { auth } from '@/auth';
+import { getOrderById } from "@/lib/actions/order.actions";
+import { auth } from "@/auth";
+import OrderDetailsTable from "./order-details-table";
+import { ShippingAddress } from "@/types";
 
 export const metadata: Metadata = {
-  title: 'Order Details',
+  title: "Order Details",
 };
 
 const OrderDetailsPage = async (props: {
@@ -13,7 +15,6 @@ const OrderDetailsPage = async (props: {
     id: string;
   }>;
 }) => {
-
   const { id } = await props.params;
 
   const order = await getOrderById(id);
@@ -21,7 +22,16 @@ const OrderDetailsPage = async (props: {
 
   const session = await auth();
 
-  return <>Details {order.totalPrice}</>;
-}
- 
+  return (
+    <>
+      <OrderDetailsTable
+        order={{
+          ...order,
+          shippingAddress: order.shippingAddress as ShippingAddress,
+        }}
+      />
+    </>
+  );
+};
+
 export default OrderDetailsPage;
